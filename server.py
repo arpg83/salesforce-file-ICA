fc45380b2475a2fd2a912aac5b39fb9f7b24dc83
Marcelo2025-public <userdevelopment2@gmail.com>
2025-10-29 23:25:42 -0300

    Update server.py

--- server.py
+++ server.py
@@ -39,9 +39,8 @@ except Exception as e:
 template_env = Environment(loader=FileSystemLoader("templates"))
 
 app = Flask(__name__)
-#CORS(app)
+CORS(app)
 #CORS(app, resources={r"/api/*": {"origins": "https://browse-file-ica.onrender.com/"}}) 
-CORS(app, resources={r"/api/*": {"origins": "*"}}) 
 api = Api(app)
 asgi_app = WSGIMiddleware(app)
 app.config['UPLOAD_FOLDER'] = './upload_file' # Carpeta donde se guardarán los archivos
@@ -154,6 +153,7 @@ def upload_pdf():
     #        'status': status_code
     #    }), status_code
     
+
 
 
 
